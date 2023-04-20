08ad45559c2f91023beb1cb725cea3eb83768dbf
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-20 22:00:51 +0000

    Modified the utf-8 validation script to handle each byte sequence individually

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,16 +4,30 @@
 
 def validUTF8(data):
     """Check if a data set is a valid utf-8 encoding."""
-    for i in range(len(data)):
+    i = 0
+    while i < (len(data)):
         if data[i] <= 0x7F:
-            continue
-        return check_bytes(data, i)
+            i += 1
+        elif data[i] >= 0xC2 or data[i] <= 0xDF:
+            if not check_bytes(data, i + 1, 2):
+                return False
+            i += 2
+        elif data[i] >= 0xE0 or data[i] <= 0xEF:
+            if not check_bytes(data, i + 2, 3):
+                return False
+            i += 3
+        elif data[i] >= 0xF0 or data[i] <= 0xF4:
+            if not check_bytes(data, i + 3, 4):
+                return False
+            i += 4
+        else:
+            return False
     return True
 
 
-def check_bytes(data, s):
+def check_bytes(data, s, e):
     """Check if a data set is a valid 2 - 4characters utf-8 encoding."""
-    for i in range(s, len(data)):
+    for i in range(s, e):
         if data[i] < 0x80 or data[i] > 0xBF:
             return False
     return True
