bd759d13228a62722b22d98e56a0a4d007751c48
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-21 00:08:59 +0000

    Modified the utf-8 validation script by checking if the range exceed the data set len

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,16 +8,16 @@ def validUTF8(data):
     while i < (len(data)):
         if data[i] <= 0x7F:
             i += 1
-        elif data[i] >= 0xC2 and data[i] <= 0xDF:
-            if not check_bytes(data, i + 1, i + 2):
+        elif (data[i] >= 0xC2) and (data[i] <= 0xDF):
+            if (i + 2 > len(data)) or check_bytes(data, i + 1, i + 2):
                 return False
             i += 2
-        elif data[i] >= 0xE0 and data[i] <= 0xEF:
-            if not check_bytes(data, i + 2, i + 3):
+        elif (data[i] >= 0xE0) and (data[i] <= 0xEF):
+            if (i + 3 > len(data)) or not check_bytes(data, i + 1, i + 3):
                 return False
             i += 3
-        elif data[i] >= 0xF0 and data[i] <= 0xF4:
-            if not check_bytes(data, i + 3, i + 4):
+        elif (data[i] >= 0xF0) and (data[i] <= 0xF4):
+            if (i + 4 > len(data)) or not check_bytes(data, i + 1, i + 4):
                 return False
             i += 4
         else:
@@ -28,6 +28,6 @@ def validUTF8(data):
 def check_bytes(data, s, e):
     """Check if a data set is a valid 2 - 4characters utf-8 encoding."""
     for i in range(s, e):
-        if data[i] < 0x80 or data[i] > 0xBF:
+        if not ((data[i] >= 0x80) and (data[i] <= 0xBF)):
             return False
     return True
