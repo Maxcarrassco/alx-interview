9a03e6c36aab24966daed99aa5d64769a6bc1ed7
Maxwell D. Dorliea <deboy170@gmail.com>
2023-06-01 12:53:11 +0000

    Change the implementation from oop to functional programming

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,45 @@
 #!/usr/bin/python3
 """ALX SE Interview Prep Module."""
 
-class PrimeGamer:
-    def __init__(self):
-        self.players = ('Maria', 'Ben')
-
-    def isPrime(self, num):
-        if num == 1:
-            return False
-        for n in range(2, (num // 2) + 1):
-            if num % 2 == 0:
-                return False
-        return True
-
-    def choosePrime(self, arr):
-        for n in arr:
-            if self.isPrime(n):
-                return n
+def isPrime(num):
+    if num == 1:
         return False
+    for n in range(2, (num // 2) + 1):
+        if num % 2 == 0:
+            return False
+    return True
 
-    def removeMultiples(self, n, arr):
-        out = []
-        for num in arr:
-            if num % n != 0:
-                out.append(num)
-        return out
+def choosePrime(arr):
+    for num in arr:
+        if isPrime(num):
+            return num
+    return False
 
-    def getPlayer(self, idx):
-        if idx % 2 == 1:
-            return self.players[0]
-        return self.players[1]
+def removeMultiples(n, arr):
+    out = set()
+    for num in arr:
+        if num % n != 0:
+            out.add(num)
+    return out
+
+def getPlayer(idx):
+    if idx % 2 == 1:
+        return 'Maria'
+    return 'Ben'
 
 
 def isWinner(x, nums):
     """Solve the Prime Game Challenge."""
     stats = {'Maria': 0, 'Ben': 0}
-    prime = PrimeGamer()
 
-    for r in range(x):
-        arr = list(range(1, nums[r] + 1))
+    for turn in range(x):
+        arr = set(range(1, nums[turn] + 1))
         for i in arr:
-            player = prime.getPlayer(i)
-            p_val = prime.choosePrime(arr)
-            if p_val:
-                arr = prime.removeMultiples(p_val, arr)
+            prime = choosePrime(arr)
+            if prime:
+                arr = removeMultiples(prime, arr)
             else:
-                winner = prime.getPlayer(i + 1)
+                winner = getPlayer(i + 1)
                 stats[winner] = stats[winner] + 1
                 break
     if stats['Maria'] == stats['Ben']:
