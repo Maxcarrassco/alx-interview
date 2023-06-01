3d3538855fd3f53e5640501384f832591e5952bb
Maxwell D. Dorliea <deboy170@gmail.com>
2023-06-01 12:35:38 +0000

    Created a script that solve the Prime Game coding interview problem

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+class PrimeGamer:
+    def __init__(self):
+        self.players = ('Maria', 'Ben')
+
+    def isPrime(self, num):
+        if num == 1:
+            return False
+        for n in range(2, (num // 2) + 1):
+            if num % 2 == 0:
+                return False
+        return True
+
+    def choosePrime(self, arr):
+        for n in arr:
+            if self.isPrime(n):
+                return n
+        return False
+
+    def removeMultiples(self, n, arr):
+        out = []
+        for num in arr:
+            if num % n != 0:
+                out.append(num)
+        return out
+
+    def getPlayer(self, idx):
+        if idx % 2 == 1:
+            return self.players[0]
+        return self.players[1]
+
+
+def isWinner(x, nums):
+    stats = {'Maria': 0, 'Ben': 0}
+    prime = PrimeGamer()
+
+    for r in range(x):
+        arr = list(range(1, nums[r] + 1))
+        for i in arr:
+            player = prime.getPlayer(i)
+            p_val = prime.choosePrime(arr)
+            if p_val:
+                arr = prime.removeMultiples(p_val, arr)
+            else:
+                winner = prime.getPlayer(i + 1)
+                stats[winner] = stats[winner] + 1
+                break
+    return 'Maria' if stats['Maria'] > stats['Ben'] else 'Ben'
